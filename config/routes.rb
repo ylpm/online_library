cc0f695e38abfeb26e356d1acc06a2da5fab1cb6
ylp82 <ylp82@yahoo.com>
2024-07-26 19:51:26 +0200

    Add About page to Static Pages by TDD

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   
   get :help, to: 'static_pages#help'
   
+  get :about, to: 'static_pages#about'
+  
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,23 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "should get home page" do
     # get static_pages_home_url
     get root_url  # get '/'
     assert_response 200 # :success
+    assert_select 'h1', 'Sample App'
   end
 
-  test "should get help" do
+  test "should get help page" do
     # get static_pages_help_url
     get help_url # get '/help'
     assert_response :success
+    assert_select 'h1', 'Help'
+  end
+  
+  test "should get about page" do
+    get about_url
+    assert_response :success
+    assert_select 'h1', 'About'
   end
 end
