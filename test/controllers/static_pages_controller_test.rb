8685b68079790ece23e7d45cbd915d8cf7687fc2
ylp82 <ylp82@yahoo.com>
2024-07-26 19:25:36 +0200

    Fix the current test to use the new helpers root_url and help_url

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+    # get static_pages_home_url
+    get root_url  # get '/'
+    assert_response 200 # :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    # get static_pages_help_url
+    get help_url # get '/help'
     assert_response :success
   end
 end
