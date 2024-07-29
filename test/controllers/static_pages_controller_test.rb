fe000fcefa877e8ae76d6d7354b310c37f04b811
ylp82 <ylp82@yahoo.com>
2024-07-29 14:38:44 +0200

    Add the full_title method helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home page" do
+  def setup
+    @base_title = 'Sample App'
+  end
+  test "should get root/home page" do
     # get static_pages_home_url
     get root_url  # get '/'
     assert_response 200 # :success
+    assert_select 'title', "Home | #{@base_title}"
     assert_select 'h1', 'Sample App'
   end
 
@@ -12,12 +16,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     # get static_pages_help_url
     get help_url # get '/help'
     assert_response :success
+    assert_select 'title', "Help | #{@base_title}"
     assert_select 'h1', 'Help'
   end
   
   test "should get about page" do
     get about_url
     assert_response :success
+    assert_select 'title', "About | #{@base_title}"
     assert_select 'h1', 'About'
   end
 end
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  def setup
+    @base_title = 'Sample App'
+    @custom_title = 'This Page'
+  end
+
+  test 'should match the base title' do
+    ['', '    '].each do |blank|
+      assert_equal @base_title, full_title(blank), "Full title should be the same as base title since no page title was provided"
+    end
+  end
+  
+  test 'should match customized page title' do
+    assert_equal "#{@custom_title} | #{@base_title}", full_title(@custom_title), "Full title should be \"#{@custom_title} | #{@base_title}\", since \"#{@custom_title}\" was provided as a page title"
+  end
+end
