c18e35d7a45b6e66d5509cd3f3c58ff40ba1abe1
ylp82 <ylp82@yahoo.com>
2024-08-15 15:24:31 +0200

    Add tests for valid and invalid signup information

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   
   resources :users
   
-  # get :signup, to: "users#new"
+  get :signup, to: "users#new"
   
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_response :success
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {person:{first_name: '1',
+                                                 last_name: '',
+                                             email_address: {address: 'foo@'}},
+                                      username: '**',
+                                      password: 'foo',
+                         password_confirmation: 'bar'}}
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div#error_count'
+    assert_select 'div.alert', 'The form contains 6 errors'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_response :success
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {person: {first_name: 'John',
+                                                 last_name: 'Johnson',
+                                             email_address: {address: 'foo@bar.com'}},
+                                     username: 'john',
+                                     password: 'Abcde123*',
+                        password_confirmation: 'Abcde123*'}}
+    end
+    assert_redirected_to User.last
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success', 'Welcome!'
+  end
+end
